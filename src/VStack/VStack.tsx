22fd10fca23a6a6d84693cb6c8f161977faa720a
everdimension <everdimension@gmail.com>
2022-10-30 14:40:40 +0700

    Forward refs for HStack and VStack

--- src/VStack/VStack.tsx
+++ src/VStack/VStack.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 
-export const VStack = ({
-  gap,
-  style,
-  ...props
-}: {
-  gap: number;
-} & React.HTMLProps<HTMLDivElement>) => {
-  return (
-    <div
-      style={{
-        display: 'grid',
-        gridGap: gap,
-        gridTemplateColumns: 'minmax(0, auto)',
-        ...style,
-      }}
-      {...props}
-    />
-  );
-};
+export const VStack = React.forwardRef(
+  (
+    {
+      gap,
+      style,
+      ...props
+    }: {
+      gap: number;
+    } & React.HTMLProps<HTMLDivElement>,
+    ref: React.Ref<HTMLDivElement>
+  ) => {
+    return (
+      <div
+        ref={ref}
+        style={{
+          display: 'grid',
+          gridGap: gap,
+          gridTemplateColumns: 'minmax(0, auto)',
+          ...style,
+        }}
+        {...props}
+      />
+    );
+  }
+);
